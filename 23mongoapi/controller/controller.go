c6d6c56f2b1d743ed6c302627dee307637236533
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-23 08:28:28 +0500

    feat(mongodb): establish database connection and collection initialization

---
+++ 23mongoapi/controller/controller.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString = "mongodb+srv://officialahmadrandhawa_db_user:roughpassword@cluster0.z6slbvm.mongodb.net/?appName=Cluster0"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// Most Important
+var collection *mongo.Collection // represent and provide access to one MongoDB collection (table).
+
+// connect with mongoDb
+
+func init() {
+	// Client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// Create client
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Verify connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB connection successful")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection instance is ready")
+}
