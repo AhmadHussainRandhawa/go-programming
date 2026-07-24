78a9f581ceaf17717d35dd31848283fa00c999a3
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-24 12:31:23 +0500

    feat(concurrency): coordinate goroutines using WaitGroup

---
+++ 25waitgroup/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	go test(1, &wg)
+	go test(2, &wg)
+	go test(3, &wg)
+
+	fmt.Println("This is main before waiting...")
+
+	wg.Wait()
+
+	fmt.Println("This is main after waiting...")
+}
+
+func test(id int, wg *sync.WaitGroup) {
+	fmt.Println("worker...", id)
+
+	defer wg.Done()
+
+}
