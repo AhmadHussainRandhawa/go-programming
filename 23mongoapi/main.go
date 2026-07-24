2b5b3aed88aed4c0c3d32f6efb52556f6a2436cc
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2026-07-24 08:28:07 +0500

    build(app): add application entry point

--- 23mongoapi/main.go
+++ 23mongoapi/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AhmadHussainRandhawa/mongoapi/router"
+)
+
+func main() {
+	fmt.Println("Server is listening...")
+
+	r := router.Router()
+	http.ListenAndServe(":4000", r)
+
+}
--- 23mongoapi/router/router.go
+++ 23mongoapi/router/router.go
@@ -9,11 +9,11 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("api/movies", controller.GetAllMovies).Methods("GET")
-	router.HandleFunc("api/movie", controller.CreateMovie).Methods("POST")
-	router.HandleFunc("api/movie/{id}", controller.MarkedAsWatched).Methods("PUT")
-	router.HandleFunc("api/movies/{id}", controller.DeleteAMovie).Methods("DELETE")
-	router.HandleFunc("api/deleteallmovie", controller.DeleteAllMovies).Methods("DELETE")
+	router.HandleFunc("/api/movies", controller.GetAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkedAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movies/{id}", controller.DeleteAMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteallmovie", controller.DeleteAllMovies).Methods("DELETE")
 
 	return router
 
